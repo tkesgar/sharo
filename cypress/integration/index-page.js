0c9fdd9dcfab7473c6d788ba5a19873b2d94c724
Tito D. Kesumo Siregar <tito.siregar@bukalapak.com>
2020-04-29 13:36:46 +0700

    Add testing with cypress

---
+++ cypress/integration/index-page.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it("should show repo card containing information about sharo", () => {
+  cy.visit("/");
+
+  cy.contains("It works!");
+
+  cy.get('[data-cy="repo-title"]').contains("sharo");
+  cy.get('[data-cy="repo-description"]').contains(
+    "❤️ Opinionated Next.js boilerplate"
+  );
+  cy.get('a[data-cy="repo-url"]')
+    .contains("tkesgar/sharo")
+    .should("have.attr", "href", "https://github.com/tkesgar/sharo");
+});
