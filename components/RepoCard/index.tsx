7a3f2e9e5b331da6a23f21567d982b13f6d3648d
Tito D. Kesumo Siregar <tito.siregar@bukalapak.com>
2020-04-29 13:36:17 +0700

    Add sample page with simple component example

---
+++ components/RepoCard/index.tsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import styles from "./styles.module.scss";
+
+interface ApiRepoData {
+  name: string;
+  full_name: string;
+  description: string;
+  html_url: string;
+  open_issues: number;
+  stargazers_count: number;
+}
+
+interface RepoCardProps extends React.ComponentProps<"div"> {
+  owner: string;
+  repo: string;
+}
+
+export default function RepoCard({
+  owner,
+  repo,
+  ...restProps
+}: RepoCardProps): JSX.Element {
+  const [apiRepoData, setApiRepoData] = React.useState<ApiRepoData>(null);
+  const [error, setError] = React.useState<string>(null);
+  React.useEffect(() => {
+    const abortController = new AbortController();
+
+    (async (): Promise<void> => {
+      try {
+        const response = await fetch(
+          `https://api.github.com/repos/${owner}/${repo}`,
+          {
+            signal: abortController.signal,
+          }
+        );
+
+        const data = await response.json();
+        setApiRepoData(data);
+      } catch (error) {
+        if (abortController.signal.aborted) {
+          return;
+        }
+
+        throw error;
+      }
+    })().catch((error) => setError(error.message));
+
+    return (): void => {
+      abortController.abort();
+    };
+  }, []);
+
+  if (!apiRepoData) {
+    return (
+      <div className={styles.RepoCard} {...restProps}>
+        <div style={{ textAlign: "center" }}>Loading...</div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className={styles.RepoCard} {...restProps}>
+        <div style={{ textAlign: "center" }}>{error}</div>
+      </div>
+    );
+  }
+
+  const {
+    name,
+    full_name: fullName,
+    description,
+    html_url: url,
+    open_issues: issues,
+    stargazers_count: stars,
+  } = apiRepoData;
+
+  return (
+    <div className={styles.RepoCard} {...restProps}>
+      <h2 data-cy="repo-title">{name}</h2>
+      <p data-cy="repo-description">{description}</p>
+      <dl>
+        <dt>Issues</dt>
+        <dd>{issues}</dd>
+        <dt>Stars</dt>
+        <dd>{stars}</dd>
+        <dt>View on GitHub</dt>
+        <dd>
+          <a href={url} data-cy="repo-url">
+            {fullName}
+          </a>
+        </dd>
+      </dl>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import RepoCard from "../components/RepoCard";
 
 export default function IndexPage(): JSX.Element {
   return (
@@ -6,16 +7,26 @@ export default function IndexPage(): JSX.Element {
       style={{
         maxWidth: "40rem",
         margin: "2rem auto 0",
-        padding: "2rem",
-        background: "#fff",
       }}
     >
-      <h1>It works!</h1>
+      <h1 style={{ textAlign: "center" }}>It works!</h1>
       <p>
-        Application environment: <code>{process.env.APP_ENV}</code>
+        The component <code>RepoCard</code> below is a simple dynamic component
+        to provide something that can be tested. You can run{" "}
+        <code>npm test</code> to run the component test (using Jest) and{" "}
+        <code>npm run cypress -- run</code> to run the functional test (using
+        Cypress).
       </p>
+      <RepoCard
+        owner="tkesgar"
+        repo="sharo"
+        style={{ width: "20rem", margin: "0 auto" }}
+      />
       <p>
-        Node environment: <code>{process.env.NODE_ENV}</code>
+        You can safely remove this page and <code>components/RepoCard</code>,
+        then start building your website. You can also visit{" "}
+        <a href="https://github.com/tkesgar/sharo#README">sharo readme</a> for
+        more details about what is included in sharo.
       </p>
     </div>
   );
