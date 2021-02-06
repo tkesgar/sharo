2ebdf3fe3d7a95cd90924bc47e0b051ef58a0a58
Ted Kesgar <t.kesgar@hey.com>
2021-02-06 10:55:29 +0700

    Remove @tkesgar/reno

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 const withSharo = require("@tkesgar/sharo-next");
-const { loadEnv, getPrefixEnvs } = require("@tkesgar/reno");
 
-loadEnv();
-
-module.exports = withSharo({
-  env: getPrefixEnvs("APP_"),
-});
+module.exports = withSharo();
